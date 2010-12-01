5c518d572d8224accfc99c29472a7ace79dd1baa
cifani.thiago <cifani.thiago@af58ab6c-5581-156b-659d-6e3732790119>
2010-12-01 18:43:52 +0000

    added formaPagamento atributo data_vencimento em pagamento Contas a Pagar criado

---
+++ trunk/CondominioPlus/src/condominioPlus/negocio/financeiro/ContaPagar.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package condominioPlus.negocio.financeiro;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Administrador
+ */
+@Entity
+public class ContaPagar {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int codigo;
+    @OneToMany
+    private List<Pagamento>pagamentos;
+
+
+}
