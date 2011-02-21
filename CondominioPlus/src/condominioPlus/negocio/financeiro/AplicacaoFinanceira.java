4779ee77c5ed5d2b76036bbb94146c8bae94388f
cifani.thiago <cifani.thiago@af58ab6c-5581-156b-659d-6e3732790119>
2011-02-21 20:22:50 +0000

    criada classe aplicacaoFinanceira

--- CondominioPlus/src/condominioPlus/negocio/financeiro/AplicacaoFinanceira.java
+++ CondominioPlus/src/condominioPlus/negocio/financeiro/AplicacaoFinanceira.java
@@ -2,13 +2,72 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package condominioPlus.negocio.financeiro;
+
+import condominioPlus.negocio.Condominio;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 /**
  *
  * @author Administrador
  */
-public class AplicacaoFinanceira {
+@Entity
+@Table(name="aplicacao_financeira")
+public class AplicacaoFinanceira implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int codigo;
+    @OneToMany(mappedBy = "aplicacao", cascade = CascadeType.ALL)
+    private List<Pagamento> pagamentos = new ArrayList<Pagamento>();
+    @Column(precision = 20, scale = 2)
+    private BigDecimal saldo = new BigDecimal(0);
+    @OneToOne
+    private Condominio condominio;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Condominio getCondominio() {
+        return condominio;
+    }
+
+    public void setCondominio(Condominio condominio) {
+        this.condominio = condominio;
+    }
+
+    public List<Pagamento> getPagamentos() {
+        return pagamentos;
+    }
+
+    public void setPagamentos(List<Pagamento> pagamentos) {
+        this.pagamentos = pagamentos;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    
 }
