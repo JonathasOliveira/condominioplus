b31c6fb5753a5d945bb051084b66287c2ea6b988
cifani.thiago <cifani.thiago@af58ab6c-5581-156b-659d-6e3732790119>
2011-02-23 20:03:23 +0000

    mudancas

--- CondominioPlus/src/condominioPlus/negocio/financeiro/AplicacaoFinanceira.java
+++ CondominioPlus/src/condominioPlus/negocio/financeiro/AplicacaoFinanceira.java
@@ -5,9 +5,12 @@
 package condominioPlus.negocio.financeiro;
 
 import condominioPlus.negocio.Condominio;
+import condominioPlus.util.ComparadorPagamentoCodigo;
+import condominioPlus.util.ComparatorPagamento;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -18,13 +21,14 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import logicpoint.persistencia.DAO;
 
 /**
  *
  * @author Administrador
  */
 @Entity
-@Table(name="aplicacao_financeira")
+@Table(name = "aplicacao_financeira")
 public class AplicacaoFinanceira implements Serializable {
 
     @Id
@@ -69,5 +73,38 @@ public class AplicacaoFinanceira implements Serializable {
         this.saldo = saldo;
     }
 
-    
+    public void adicionarPagamento(Pagamento pagamento) {
+        if (pagamento != null) {
+            pagamento.setAplicacao(this);
+            pagamentos.add(pagamento);
+        }
+    }
+
+    public void calculaSaldo() {
+
+
+        List<Pagamento> pagamentos = new DAO().listar(Pagamento.class, "PagamentosAplicacaoOrdenados", this);
+
+        ComparadorPagamentoCodigo comCod = new ComparadorPagamentoCodigo();
+
+        Collections.sort(pagamentos, comCod);
+
+        ComparatorPagamento comparator = new ComparatorPagamento();
+
+        Collections.sort(pagamentos, comparator);
+
+        for (int i = 0; i < pagamentos.size(); i++) {
+            if (i != 0) {
+                Pagamento p1 = pagamentos.get(i);
+                Pagamento pagamentoAnterior = pagamentos.get(i - 1);
+                p1.setSaldo(pagamentoAnterior.getSaldo().add(p1.getValor()));
+
+                condominio.getPoupanca().setSaldo(p1.getSaldo());
+            }
+
+        }
+
+        new DAO().salvar(pagamentos);
+
+    }
 }
