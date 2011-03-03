cdb7fffcebe10ed5f612756a76a97ddbf7393c4d
cifani.thiago <cifani.thiago@af58ab6c-5581-156b-659d-6e3732790119>
2011-03-03 15:38:09 +0000

    criacao classe ContratoEmprestimo e atributos em Emprestimo

--- trunk/CondominioPlus/src/condominioPlus/negocio/financeiro/Emprestimo.java
+++ trunk/CondominioPlus/src/condominioPlus/negocio/financeiro/Emprestimo.java
@@ -5,10 +5,21 @@
 
 package condominioPlus.negocio.financeiro;
 
+import condominioPlus.negocio.Condominio;
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  *
  * @author Administrador
  */
 public class Emprestimo {
+
+    private int codigo;
+    private Condominio condominio;
+    private List<ContratoEmprestimo> contratos;
+    private BigDecimal saldo;
+
+
 
 }
