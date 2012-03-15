634d35b239d9dd294b1bbbdaca9039cbd3ba86be
gegeemfb@gmail.com <gegeemfb@gmail.com@af58ab6c-5581-156b-659d-6e3732790119>
2012-03-15 18:32:57 +0000

    Relatorio Inadimplencia Sintetica / Inicio Relatorio Inadimplencia Analitica

--- trunk/CondominioPlus/src/condominioPlus/util/Relatorios.java
+++ trunk/CondominioPlus/src/condominioPlus/util/Relatorios.java
@@ -8,17 +8,26 @@
  */
 package condominioPlus.util;
 
+import condominioPlus.negocio.Condominio;
+import condominioPlus.negocio.NegocioUtil;
+import condominioPlus.negocio.Unidade;
+import condominioPlus.negocio.cobranca.Cobranca;
+import condominioPlus.negocio.financeiro.PagamentoUtil;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import javax.swing.JOptionPane;
 import logicpoint.util.DataUtil;
+import logicpoint.util.Moeda;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -126,5 +135,89 @@ public class Relatorios implements Printable {
                 ex.printStackTrace();
             }
         }
+    }
+
+    public void imprimirRelatorioInadimplencia(Condominio condominio, DateTime dataInicial, DateTime dataFinal) {
+        List<HashMap<String, Object>> lista = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> parametros = new HashMap();
+
+        UNIDADES:
+        for (Unidade u : condominio.getUnidades()) {
+
+            List<HashMap<String, String>> listaCobrancas = new ArrayList<HashMap<String, String>>();
+
+            parametros.put("periodo", DataUtil.toString(dataInicial) + " a " + DataUtil.toString(dataFinal));
+            parametros.put("condominio", condominio.getRazaoSocial());
+
+            BigDecimal totalOriginal = new BigDecimal(0);
+            BigDecimal totalJuros = new BigDecimal(0);
+            BigDecimal totalMulta = new BigDecimal(0);
+            BigDecimal totalGeral = new BigDecimal(0);
+            for (Cobranca co : u.getCobrancas()) {
+                Cobranca cobrancaAux = new Cobranca();
+                cobrancaAux.setValorOriginal(co.getValorOriginal());
+                cobrancaAux.setJuros(co.getJuros());
+                cobrancaAux.setMulta(co.getMulta());
+                cobrancaAux.setValorTotal(co.getValorTotal());
+                cobrancaAux.setDataVencimento(co.getDataVencimento());
+                cobrancaAux.setDataPagamento(co.getDataPagamento());
+                cobrancaAux.setExibir(co.isExibir());
+                cobrancaAux.setNumeroDocumento(co.getNumeroDocumento());
+                if (cobrancaAux.getDataPagamento() == null && DataUtil.compararData(dataInicial, DataUtil.getDateTime(cobrancaAux.getDataVencimento())) == -1 && DataUtil.compararData(dataFinal, DataUtil.getDateTime(cobrancaAux.getDataVencimento())) == 1 && cobrancaAux.isExibir()) {
+                    calcularJurosMulta(cobrancaAux, dataFinal);
+                    HashMap<String, String> mapa = new HashMap();
+                    totalOriginal = totalOriginal.add(cobrancaAux.getValorOriginal());
+                    totalJuros = totalJuros.add(cobrancaAux.getJuros());
+                    totalMulta = totalMulta.add(cobrancaAux.getMulta());
+                    totalGeral = totalGeral.add(cobrancaAux.getValorTotal());
+                    mapa.put("documento", cobrancaAux.getNumeroDocumento());
+                    mapa.put("vencimento", DataUtil.toString(cobrancaAux.getDataVencimento()));
+                    mapa.put("valorOriginal", PagamentoUtil.formatarMoeda(cobrancaAux.getValorOriginal().doubleValue()));
+                    mapa.put("juros", PagamentoUtil.formatarMoeda(cobrancaAux.getJuros().doubleValue()));
+                    mapa.put("multa", PagamentoUtil.formatarMoeda(cobrancaAux.getMulta().doubleValue()));
+                    mapa.put("total", PagamentoUtil.formatarMoeda(cobrancaAux.getValorTotal().doubleValue()));
+                    listaCobrancas.add(mapa);
+                }
+            }
+            if (listaCobrancas.isEmpty()) {
+                continue UNIDADES;
+            } else {
+                HashMap<String, Object> mapa2 = new HashMap();
+                mapa2.put("unidade", u.getUnidade());
+                mapa2.put("nome", u.getCondomino().getNome());
+                mapa2.put("totalOriginal", PagamentoUtil.formatarMoeda(totalOriginal.doubleValue()));
+                mapa2.put("totalJuros", PagamentoUtil.formatarMoeda(totalJuros.doubleValue()));
+                mapa2.put("totalMulta", PagamentoUtil.formatarMoeda(totalMulta.doubleValue()));
+                mapa2.put("totalGeral", PagamentoUtil.formatarMoeda(totalGeral.doubleValue()));
+                mapa2.put("lista", new JRBeanCollectionDataSource(listaCobrancas));
+                lista.add(mapa2);
+            }
+
+            URL caminho = getClass().getResource("/condominioPlus/relatorios/");
+            parametros.put("subrelatorio", caminho.toString());
+
+        }
+
+        imprimir("InadimplenciaSintetica", parametros, lista, false);
+    }
+
+    private void calcularJurosMulta(Cobranca cobranca, DateTime dataProrrogada) {
+        Moeda juros = new Moeda();
+        Moeda multa = new Moeda();
+        cobranca.setValorTotal(new BigDecimal(0));
+        cobranca.setValorTotal(cobranca.getValorTotal().add(cobranca.getValorOriginal()));
+        double diferencaMeses = 0;
+        diferencaMeses = DataUtil.getDiferencaEmMeses(dataProrrogada, DataUtil.getDateTime(cobranca.getDataVencimento()));
+        if (diferencaMeses > 0) {
+//            System.out.println("diferenca meses: " + new Double(diferencaMeses).intValue());
+            juros.soma(new Double(diferencaMeses).intValue()).multiplica(NegocioUtil.getConfiguracao().getPercentualJuros().divide(new BigDecimal(100)));
+//            System.out.println("juros: " + juros);
+            juros.multiplica(cobranca.getValorTotal());
+            multa.soma(NegocioUtil.getConfiguracao().getPercentualMulta().divide(new BigDecimal(100)));
+            multa.multiplica(cobranca.getValorTotal());
+        }
+        cobranca.setJuros(juros.bigDecimalValue().setScale(2, RoundingMode.UP));
+        cobranca.setMulta(multa.bigDecimalValue().setScale(2, RoundingMode.UP));
+        cobranca.setValorTotal(cobranca.getValorTotal().add(cobranca.getJuros().add(cobranca.getMulta())).setScale(2, RoundingMode.UP));
     }
 }
