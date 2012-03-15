ed3286a3f2b6e4038de68966070b717b975a9d9b
gegeemfb@gmail.com <gegeemfb@gmail.com@af58ab6c-5581-156b-659d-6e3732790119>
2012-03-15 18:32:18 +0000

    Relatorio Inadimplencia Sintetica / Inicio Relatorio Inadimplencia Analitica

--- trunk/CondominioPlus/src/condominioPlus/apresentacao/cobranca/TelaDadosRelatorioGerencial.java
+++ trunk/CondominioPlus/src/condominioPlus/apresentacao/cobranca/TelaDadosRelatorioGerencial.java
@@ -11,18 +11,12 @@
 package condominioPlus.apresentacao.cobranca;
 
 import condominioPlus.negocio.Condominio;
-import condominioPlus.negocio.NegocioUtil;
-import condominioPlus.negocio.Unidade;
-import condominioPlus.negocio.cobranca.BoletoBancario;
-import condominioPlus.negocio.cobranca.Cobranca;
 import condominioPlus.negocio.cobranca.taxaExtra.ParcelaTaxaExtra;
 import condominioPlus.negocio.cobranca.taxaExtra.RateioTaxaExtra;
 import condominioPlus.negocio.cobranca.taxaExtra.TaxaExtra;
 import condominioPlus.negocio.financeiro.PagamentoUtil;
 import condominioPlus.util.Relatorios;
 import java.awt.event.ActionEvent;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -42,6 +36,7 @@ public class TelaDadosRelatorioGerencial extends javax.swing.JInternalFrame {
 
     private DateTime dataInicial;
     private DateTime dataFinal;
+    private DateTime dataCalculoJurosMulta;
     private Condominio condominio;
 
     /** Creates new form TelaDadosRelatorioGerencial */
@@ -145,91 +140,11 @@ public class TelaDadosRelatorioGerencial extends javax.swing.JInternalFrame {
 
             } else {
 
-                List<HashMap<String, Object>> lista = new ArrayList<HashMap<String, Object>>();
-                HashMap<String, Object> parametros = new HashMap();
-
-                UNIDADES:
-                for (Unidade u : condominio.getUnidades()) {
-
-                    List<HashMap<String, String>> listaCobrancas = new ArrayList<HashMap<String, String>>();
-
-                    parametros.put("periodo", DataUtil.toString(dataInicial) + " a " + DataUtil.toString(dataFinal));
-                    parametros.put("condominio", condominio.getRazaoSocial());
-
-                    BigDecimal totalOriginal = new BigDecimal(0);
-                    BigDecimal totalJuros = new BigDecimal(0);
-                    BigDecimal totalMulta = new BigDecimal(0);
-                    BigDecimal totalGeral = new BigDecimal(0);
-                    for (Cobranca co : u.getCobrancas()) {                        
-                        Cobranca cobrancaAux = new Cobranca();
-                        cobrancaAux.setValorOriginal(co.getValorOriginal());
-                        cobrancaAux.setJuros(co.getJuros());
-                        cobrancaAux.setMulta(co.getMulta());
-                        cobrancaAux.setValorTotal(co.getValorTotal());
-                        cobrancaAux.setDataVencimento(co.getDataVencimento());
-                        cobrancaAux.setDataPagamento(co.getDataPagamento());
-                        cobrancaAux.setExibir(co.isExibir());
-                        cobrancaAux.setNumeroDocumento(co.getNumeroDocumento());                        
-                        if (cobrancaAux.getDataPagamento() == null && DataUtil.compararData(dataInicial, DataUtil.getDateTime(cobrancaAux.getDataVencimento())) == -1 && DataUtil.compararData(dataFinal, DataUtil.getDateTime(cobrancaAux.getDataVencimento())) == 1 && cobrancaAux.isExibir()) {
-                            calcularJurosMulta(cobrancaAux, dataFinal);
-                            HashMap<String, String> mapa = new HashMap();
-                            totalOriginal = totalOriginal.add(cobrancaAux.getValorOriginal());
-                            totalJuros = totalJuros.add(cobrancaAux.getJuros());
-                            totalMulta = totalMulta.add(cobrancaAux.getMulta());
-                            totalGeral = totalGeral.add(cobrancaAux.getValorTotal());
-                            mapa.put("documento", cobrancaAux.getNumeroDocumento());
-                            mapa.put("vencimento", DataUtil.toString(cobrancaAux.getDataVencimento()));
-                            mapa.put("valorOriginal", PagamentoUtil.formatarMoeda(cobrancaAux.getValorOriginal().doubleValue()));
-                            mapa.put("juros", PagamentoUtil.formatarMoeda(cobrancaAux.getJuros().doubleValue()));
-                            mapa.put("multa", PagamentoUtil.formatarMoeda(cobrancaAux.getMulta().doubleValue()));
-                            mapa.put("total", PagamentoUtil.formatarMoeda(cobrancaAux.getValorTotal().doubleValue()));
-                            listaCobrancas.add(mapa);
-                        }
-                    }
-                    if (listaCobrancas.isEmpty()) {
-                        continue UNIDADES;
-                    } else {
-                        HashMap<String, Object> mapa2 = new HashMap();
-                        mapa2.put("unidade", u.getUnidade());
-                        mapa2.put("nome", u.getCondomino().getNome());
-                        mapa2.put("totalOriginal", PagamentoUtil.formatarMoeda(totalOriginal.doubleValue()));
-                        mapa2.put("totalJuros", PagamentoUtil.formatarMoeda(totalJuros.doubleValue()));
-                        mapa2.put("totalMulta", PagamentoUtil.formatarMoeda(totalMulta.doubleValue()));
-                        mapa2.put("totalGeral", PagamentoUtil.formatarMoeda(totalGeral.doubleValue()));
-                        mapa2.put("lista", new JRBeanCollectionDataSource(listaCobrancas));
-                        lista.add(mapa2);
-                    }
-
-                    URL caminho = getClass().getResource("/condominioPlus/relatorios/");
-                    parametros.put("subrelatorio", caminho.toString());
-
-                }
-
-                new Relatorios().imprimir("InadimplenciaSintetica", parametros, lista, false);
+                new Relatorios().imprimirRelatorioInadimplencia(condominio, dataInicial, dataFinal);
 
             }
         }
-    }
-
-    private void calcularJurosMulta(Cobranca cobranca, DateTime dataProrrogada) {
-        Moeda juros = new Moeda();
-        Moeda multa = new Moeda();
-        cobranca.setValorTotal(new BigDecimal(0));
-        cobranca.setValorTotal(cobranca.getValorTotal().add(cobranca.getValorOriginal()));
-        double diferencaMeses = 0;
-        diferencaMeses = DataUtil.getDiferencaEmMeses(dataProrrogada, DataUtil.getDateTime(cobranca.getDataVencimento()));
-        if (diferencaMeses > 0) {
-//            System.out.println("diferenca meses: " + new Double(diferencaMeses).intValue());
-            juros.soma(new Double(diferencaMeses).intValue()).multiplica(NegocioUtil.getConfiguracao().getPercentualJuros().divide(new BigDecimal(100)));
-//            System.out.println("juros: " + juros);
-            juros.multiplica(cobranca.getValorTotal());
-            multa.soma(NegocioUtil.getConfiguracao().getPercentualMulta().divide(new BigDecimal(100)));
-            multa.multiplica(cobranca.getValorTotal());
-        }
-        cobranca.setJuros(juros.bigDecimalValue().setScale(2, RoundingMode.UP));
-        cobranca.setMulta(multa.bigDecimalValue().setScale(2, RoundingMode.UP));
-        cobranca.setValorTotal(cobranca.getValorTotal().add(cobranca.getJuros().add(cobranca.getMulta())).setScale(2, RoundingMode.UP));
-    }
+    }    
 
     private class ControladorEventos extends ControladorEventosGenerico {
 
@@ -269,6 +184,8 @@ public class TelaDadosRelatorioGerencial extends javax.swing.JInternalFrame {
         txtDataFinal = new net.sf.nachocalendar.components.DateField();
         txtDataInicial = new net.sf.nachocalendar.components.DateField();
         jLabel3 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        txtDataCaluloJurosMulta = new net.sf.nachocalendar.components.DateField();
 
         setClosable(true);
 
@@ -304,6 +221,8 @@ public class TelaDadosRelatorioGerencial extends javax.swing.JInternalFrame {
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("a");
 
+        jLabel1.setText("Calcular juros/multa até:");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -311,15 +230,20 @@ public class TelaDadosRelatorioGerencial extends javax.swing.JInternalFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtDataInicial, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 12, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtDataFinal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(txtDataInicial, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 12, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txtDataFinal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(txtDataCaluloJurosMulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -332,27 +256,29 @@ public class TelaDadosRelatorioGerencial extends javax.swing.JInternalFrame {
                     .addComponent(txtDataInicial, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtDataFinal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addComponent(txtDataCaluloJurosMulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 336, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 141, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(12, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -360,10 +286,12 @@ public class TelaDadosRelatorioGerencial extends javax.swing.JInternalFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnOk;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private net.sf.nachocalendar.components.DateField txtDataCaluloJurosMulta;
     private net.sf.nachocalendar.components.DateField txtDataFinal;
     private net.sf.nachocalendar.components.DateField txtDataInicial;
     // End of variables declaration//GEN-END:variables
