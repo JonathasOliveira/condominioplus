d6293be379d325bb41ca82f89edd1086282ed1dd
gegeemfb <gegeemfb@gmail.com>
2013-09-27 16:16:03 +0000

    Impressao envelopes de acordo com as configuracoes de correspondencia (proprietario e/ou inquilino)

---
+++ CondominioPlus/src/condominioPlus/negocio/cobranca/DadosCorrespondencia.java
@@ -0,0 +1,113 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condominioPlus.negocio.cobranca;
+
+/**
+ *
+ * @author eugenia
+ */
+public class DadosCorrespondencia {
+    
+    private String condominio;
+    private String unidade;
+    private String nome;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private boolean inquilino;
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public boolean isInquilino() {
+        return inquilino;
+    }
+
+    public void setInquilino(boolean inquilino) {
+        this.inquilino = inquilino;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public String getCondominio() {
+        return condominio;
+    }
+
+    public void setCondominio(String condominio) {
+        this.condominio = condominio;
+    }
+    
+}
